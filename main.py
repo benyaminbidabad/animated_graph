c3c853b35b61bf45b44b4ea02a5f945b8c148d93
Benyaminbidabad@gmail.com <Benyaminbidabad@gmail.com>
2024-08-24 20:08:05 +0200

    Finished

--- main.py
+++ main.py
@@ -12,23 +12,28 @@ df["Males"]=df["Males"].str.replace(",","").astype('int')
 df["Females"]=df["Females"].str.replace(",","").astype('int')
 df['Females']=df['Females']*-1
 
-filtered=df[df["Year"]==2000]
+
 fig, ax=plt.subplots(figsize=(15,8))
-ax.set_xlim(-2_000_000,2_000_000)
-males=plt.barh(y=filtered["Age Group"], width=filtered["Males"],color="blue")
-females=plt.barh(y=filtered["Age Group"], width=filtered["Females"],color="red")
+def animate(year):
+    ax.clear()
+    filtered=df[df["Year"]==year]
+    ax.set_xlim(-2_000_000,2_000_000)
+    males=plt.barh(y=filtered["Age Group"], width=filtered["Males"],color="blue")
+    females=plt.barh(y=filtered["Age Group"], width=filtered["Females"],color="red")
 
-ax.get_xaxis().set_visible(False)
-for edge in ["left","right","bottom","top"]:
-    ax.spines[edge].set_visible(False)
-ax.tick_params(left=False)
+    ax.get_xaxis().set_visible(False)
+    for edge in ["left","right","bottom","top"]:
+        ax.spines[edge].set_visible(False)
+    ax.tick_params(left=False)
 
-ax.bar_label(males, padding=3, labels=[f'{round(x,-3):,}' for x in filtered['Males']])
-ax.bar_label(females,padding= 3, labels=[f'{-1*round(x,-3):,}' for x in filtered['Females']])
+    ax.bar_label(males, padding=3, labels=[f'{round(x,-3):,}' for x in filtered['Males']])
+    ax.bar_label(females,padding= 3, labels=[f'{-1*round(x,-3):,}' for x in filtered['Females']])
 
-ax.legend([males,females],["Males","Females"])
+    ax.legend([males,females],["Males","Females"])
 
-ax.legend([males,females])
+    ax.set_title(f"Canada's Poulation in {year}",size=18,weight="bold")
+
+animation=FuncAnimation(fig, animate,frames=df["Year"].unique())
+animation.save("./output/Population Distribution.gif",dpi=300,writer=PillowWriter(fps=5))
 
 
-plt.show()
