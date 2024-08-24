804097d200321d1687ad38ce0adbf7d8e5f8bc42
Benyaminbidabad@gmail.com <Benyaminbidabad@gmail.com>
2024-08-24 12:27:12 +0200

    progress

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ df['Females']=df['Females']*-1
 
 filtered=df[df["Year"]==2000]
 fig, ax=plt.subplots(figsize=(15,8))
-males=plt.barh(y=filtered["Age Group"], width=filtered["Males"])
-females=plt.barh(y=filtered["Age Group"], width=filtered["Females"])
+ax.set_xlim(-2_000_000,2_000_000)
+males=plt.barh(y=filtered["Age Group"], width=filtered["Males"],color="blue")
+females=plt.barh(y=filtered["Age Group"], width=filtered["Females"],color="red")
 
+ax.get_xaxis().set_visible(False)
+for edge in ["left","right","bottom","top"]:
+    ax.spines[edge].set_visible(False)
+ax.tick_params(left=False)
 
-ax.bar_label(males,padding= 3, labels=[f'{round(x,-3):,}' for x in filtered['Males']])
+ax.bar_label(males, padding=3, labels=[f'{round(x,-3):,}' for x in filtered['Males']])
+ax.bar_label(females,padding= 3, labels=[f'{-1*round(x,-3):,}' for x in filtered['Females']])
+
+ax.legend([males,females],["Males","Females"])
+
 plt.show()
-
